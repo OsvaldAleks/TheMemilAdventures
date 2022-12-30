f1f70e8474242ebc973035972b68a46e7844c20f
OsvaldAleks <osvald.aleksander@gmail.com>
2022-12-30 15:49:07 +0100

    camera movement and bridge mechanics

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,8 +6,7 @@ public class CameraController : MonoBehaviour
 {
     GameObject player;
     public UnityEngine.Vector3 offset;
-    public float positionDamping = 1;
-    public float rotationDamping = 0.01F;
+    public float positionDamping = 0.5F;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +23,7 @@ public class CameraController : MonoBehaviour
     {
         float currentAngle = transform.eulerAngles.y;
         float desiredAngle = player.transform.eulerAngles.y;
-        float angle = Mathf.LerpAngle(desiredAngle, currentAngle, Time.deltaTime * rotationDamping);
-        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
 
         Vector3 desiredPosition = player.transform.position + (rotation * offset);
         Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * positionDamping);
